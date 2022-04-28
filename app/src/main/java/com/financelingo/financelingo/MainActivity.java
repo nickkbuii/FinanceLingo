13bb47d5937ff1dbf103ade34901b2f5e7f17f52
nickkbuii <nicholasqb01@gmail.com>
2022-04-28 10:11:32 -0700

    4/28/22 10:11 A.M

--- app/src/main/java/com/financelingo/financelingo/MainActivity.java
+++ app/src/main/java/com/financelingo/financelingo/MainActivity.java
@@ -1,14 +1,32 @@
 package com.financelingo.financelingo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button switchToCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToCreate = findViewById(R.id.createButton);
+        switchToCreate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                switchActivities();
+            }
+        });
     }
+
+    private void switchActivities(){
+        Intent switchActivityIntent = new Intent (this, create.class);
+        startActivity(switchActivityIntent);
+    }
+
 }
--- app/src/main/java/com/financelingo/financelingo/create.java
+++ app/src/main/java/com/financelingo/financelingo/create.java
@@ -1,14 +1,24 @@
 package com.financelingo.financelingo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class create extends AppCompatActivity {
+    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        create = findViewById(R.id.createButton);
+        create.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 }
