f5900c88193f02eb4d16e89d5aed3e49ab75f32c
nickkbuii <nicholasqb01@gmail.com>
2022-05-18 14:35:02 -0700

    Need to constrain

--- app/src/main/java/com/financelingo/financelingo/Lessons.java
+++ app/src/main/java/com/financelingo/financelingo/Lessons.java
@@ -1,11 +1,7 @@
 
 package com.financelingo.financelingo;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class Lessons extends AppCompatActivity{
 
@@ -13,10 +9,5 @@ public class Lessons extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lessons);
-    }
-
-    public void switchActivities(Context context, Class c){
-        Intent switchActivityIntent = new Intent (context, c);
-        startActivity(switchActivityIntent);
     }
 }
