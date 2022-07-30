b38ad77a637aedbfb46a1ec78620f4131049ca6e
brelieu05 <88119024+brelieu05@users.noreply.github.com>
2022-07-29 21:34:08 -0700

    Added email and password change buttons that disappear when pressed and will show the text edits

--- app/src/main/java/com/financelingo/financelingo/AccSettings.java
+++ app/src/main/java/com/financelingo/financelingo/AccSettings.java
@@ -3,6 +3,8 @@ package com.financelingo.financelingo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,13 +23,37 @@ import database.Database;
 
 public class AccSettings extends AppCompatActivity {
     Database db;
+    private Button changeEmailButton;
+    private Button changePasswordButton;
+    private EditText changeEmailType;
+    private EditText changePasswordType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_settings);
-
+        changeEmailButton = findViewById(R.id.changeEmail);
+        changePasswordButton = findViewById(R.id.changePassword);
+        changeEmailType = findViewById(R.id.changeEmailType);
+        changePasswordType = findViewById(R.id.changePasswordType);
         db = new Database();
+
+
+        changeEmailButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                changeEmailButton.setVisibility(View.GONE);
+                changeEmailType.setVisibility(View.VISIBLE);
+            }
+        });
+
+        changePasswordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                changePasswordButton.setVisibility(View.GONE);
+                changePasswordType.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public void changeEmail(View v){
