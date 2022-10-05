788d3a028c4c1b10e55b5b3a8058c02bfd565f4b
Nick <nicholasqb01@gmail.com>
2022-10-05 09:25:16 -0700

    Investments curriculum and UI (need to program lesson)

--- app/src/main/java/Lessons/Investments.java
+++ app/src/main/java/Lessons/Investments.java
@@ -1,4 +1,27 @@
 package Lessons;
 
 public class Investments {
+    public String[] prompts = {
+            "Investing is to purchase things that would hopefully yield profit",
+            "Investing is an effective way to build wealth despite inflation",
+            "3 investment methods include stocks, index funds, and real estate",
+            "When the value of your shares increase, so does your ownership in the company",
+            "Index funds are collections of shares from multiple different companies"
+    };
+
+    public String[][] answers = {
+            {"purchase", "things", "profit",},
+            {"effective", "wealth", "inflation"},
+            {"stocks", "index funds", "real estate"},
+            {"value", "shares", "ownership"},
+            {"collections", "multiple", "companies"}
+    };
+
+    public String[][] opts = {
+            {"things", "profit", "purchase"},
+            {"inflation", "wealth", "effective"},
+            {"stocks", "real estate", "index funds"},
+            {"value", "shares", "ownership"},
+            {"companies", "collections", "multiple"}
+    };
 }
